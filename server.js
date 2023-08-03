be9ffaff6e993022fee5aba0f192dbb8a0688ab9
ItsHans0 <125501325+ItsHans0@users.noreply.github.com>
2023-08-03 14:05:20 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,53 @@
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/serverInfo', async (req, res) => {
+  try {
+    const url = 'https://minecraftservers.org/server/652627';
+    const response = await axios.get(url);
+    const serverInfo = extractServerInfo(response.data);
+    res.json(serverInfo);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch server info' });
+  }
+});
+
 app.get('/voters', async (req, res) => {
   try {
     const url = 'https://minecraftservers.org/vote/652627';
     const response = await axios.get(url);
-    const votersMsg = extractVoters(response.data); // Mengambil hanya daftar nama voters
-    res.json({ voters: votersMsg }); // Mengirim daftar nama voters sebagai respons JSON
+    const voters = extractVoters(response.data);
+    res.json({ voters });
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch voters' });
   }
 });
+
+function extractServerInfo(html) {
+  const $ = cheerio.load(html);
+  const serverInfo = {};
+
+  serverInfo.owner = $('table.server-info tr:eq(0) td:eq(1) span').text().trim();
+  serverInfo.status = $('table.server-info tr:eq(1) td:eq(1) span.tag').text().trim();
+  serverInfo.ip = $('table.server-info tr:eq(2) td:eq(1) span').text().trim();
+  serverInfo.website = $('table.server-info tr:eq(3) td:eq(1) span a').attr('href').trim();
+  serverInfo.players = $('table.server-info tr:eq(4) td:eq(1) span').text().trim();
+  serverInfo.version = $('table.server-info tr:eq(5) td:eq(1) a').text().trim();
+  serverInfo.rank = $('table.server-info tr:eq(6) td:eq(1) span').text().trim();
+  serverInfo.votes = $('table.server-info tr:eq(7) td:eq(1) span').text().trim();
+  serverInfo.uptime = $('table.server-info tr:eq(8) td:eq(1) span').text().trim();
+  serverInfo.lastPing = $('table.server-info tr:eq(9) td:eq(1) span').text().trim();
+  serverInfo.country = $('table.server-info tr:eq(10) td:eq(1) span').text().trim();
+  serverInfo.tags = $('table.server-info tr:eq(11) td:eq(1) a').toArray().map((el) => $(el).text().trim());
+
+  return serverInfo;
+}
 
 function extractVoters(html) {
   const $ = cheerio.load(html);
@@ -20,8 +60,12 @@ function extractVoters(html) {
   const voters = [];
   votersTable.each((index, element) => {
     const voterName = $(element).find('td:eq(0)').text().trim();
-    voters.push(voterName); // Menambahkan nama voter ke array voters
+    voters.push(voterName);
   });
 
   return voters;
 }
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
